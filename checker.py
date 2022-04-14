42f62b9a585dab063c329b61bfac8c7f008ecd80
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2022-04-14 22:20:27 +0700

    List all files and directories

--- checker.py
+++ checker.py
@@ -1,8 +1,21 @@
 import os
 import sys
 
+
 def main(dir):
-    print(dir)
+    print("Working directory: " + dir)
+    counter = 10  # For debuging
+    for root, subdirs, files in os.walk(dir):
+        print('--\nroot = ' + root)
+        for subdir in subdirs:
+            print('\t- subdirectory ' + subdir)
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            print('\t- file %s (full path: %s)' % (filename, file_path))
+        counter -= 1
+        if counter == 0:
+            return
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
