b6084846733d048c5ae99000c78834d720f3bdc8
Nguyen Thanh Dung <dung.nguyenthanh@gameloft.com>
2022-04-14 22:11:45 +0700

    Get working directory from launch.json

--- checker.py
+++ checker.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import os
+import sys
+
+def main(dir):
+    print(dir)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please specify the working directory")
+    else:
+        working_dir = sys.argv[1]
+        if working_dir == None or len(working_dir) == 0:
+            print("Please specify the working directory")
+        else:
+            main(working_dir)
